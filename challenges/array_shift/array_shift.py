4206dac4b46400b82b71bea23565ea31a112269e
RAVEN DELANEY <ravend17@gmail.com>
2019-12-04 09:56:55 -0800

    code review

--- challenges/array_shift/array_shift.py
+++ challenges/array_shift/array_shift.py
@@ -7,5 +7,18 @@ def insert_shift_array(lst, val):
   for i in range(key_index, len(lst)):
     result.append(lst[i])
 
-  return result
+  # return result
+# print(insert_shift_array([1, 2, 4, 5], 3))
+
+
+def insert_array_shift(lst, val):
+  if len(lst) % 2 == 0:
+    mid = len(lst) //2
+    new_lst =lst[:mid] + [val] + lst[mid:]
+    return new_lst
+
+  elif len(lst) % 2 == 1:
+    mid = len(lst) //2 + 1
+    new_lst =lst[:mid]+[val]+lst[mid:]
+    return new_lst
 print(insert_shift_array([1, 2, 4, 5], 3))
