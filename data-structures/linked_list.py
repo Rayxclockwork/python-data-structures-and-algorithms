451046a7e3a6e2941895e4d13e12763732e5da3b
RAVEN DELANEY <ravend17@gmail.com>
2019-12-09 16:04:35 -0800

    whiteboard and some code

--- data-structures/linked_list.py
+++ data-structures/linked_list.py
@@ -1,15 +1,10 @@
 class LinkedList:
-    def __init__(self, data, next):
+    def __init__(self):
         """starts empty linked list"""
-        self.data = data
-        self.next = None
         self.head = None
 
     def includes(self, target):
         """searches linked list to find specific value"""
-        # if not self.head:
-        #     return False
-
         current = self.head
         while current:
             if current.value == target:
@@ -17,15 +12,36 @@ class LinkedList:
         current = current.next
         return False
 
-    def insert(self, item):
+    def insert(self, data):
         """Instantiates new node as head"""
-        new_node = Node(data, self.head)
-        self.head = newNode
-        self.size += 1
+        new_node = Node(data)
+        self.head = new_node
 
     def __str__(self):
         if self.size > 1:
             return str(LinkedList)
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            return
+        n = self.head
+        while n.ref is not None:
+            self.head = self.head.ref
+            self.head.ref = new_node
+
+    def insert_after(self, value, new_value):
+        while current.next:
+            if 
+
+    def insert_before(self, value, new_value):
+        current = self.head
+        while current.next:
+            if current.next.value == value:
+                new_node = Node(new_value)
+                new_node.next = current.next
+                current.next = new_node
 
 
 class Node(LinkedList):
@@ -33,12 +49,3 @@ class Node(LinkedList):
     def __init__(self, value):
         self.value = value
         self.next = None
-
-
-node1 = Node('1')
-node2 = Node('2')
-node3 = Node('3')
-node4 = Node('5')
-node5 = Node('12')
-node6 = Node('4')
-node7 = Node('15')
--- data-structures/test_linked_list.py
+++ data-structures/test_linked_list.py
@@ -2,21 +2,40 @@ from linked_list import LinkedList, Node
 import pytest
 
 
-def test_includes():
-    assert LinkedList.includes(LinkedList, 12) is True
+def test_includes(new_list):
+    assert new_list.includes(12)
+    assert not new_list.includes(45)
 
 
-def test_doesnt_include():
-    assert LinkedList.includes(LinkedList, 34) is False
+def test_string(new_list):
+    assert new_list.__str__
 
 
-def test_string():
-    assert LinkedList.__str__ == []
-
-
-def test_init():
-    assert LinkedList() == []
+# def test_init():
+#     assert LinkedList() == []
 
 
 def test_insert():
-    assert LinkedList.insert() == []
+    test_list = LinkedList()
+    test_list.insert(1)
+    assert test_list.head.value == 1
+
+
+def test_append(new_list):
+    assert new_list.append(8)
+
+
+def test_insert_before(new_list):
+    assert new_list.insert_before(5, 12)
+
+
+# def test_insert_after():
+#     assert LinkedList.insert_after(3, 8)
+
+
+@pytest.fixture()
+def new_list():
+    test_list = LinkedList()
+    for i in range(1, 20):
+        test_list.insert(i)
+    return test_list
