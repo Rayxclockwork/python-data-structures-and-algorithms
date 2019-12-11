ec79599bb8afa4a304267512705529a0bd23a353
RAVEN DELANEY <ravend17@gmail.com>
2019-12-11 14:50:03 -0800

    new branch

--- challenges/ll_merge/ll_merge.py
+++ challenges/ll_merge/ll_merge.py
@@ -1,21 +1,6 @@
 class LinkedList:
-    def __init__(self):
-        """starts empty linked list"""
-        self.head = None
+	def __init__(self):
+  """starts empty linked list"""
+    self.head = None
 
-        def merge_lists(self, ll2):
-         ll1_current = self.head
-         ll2_current = ll2.head
-
-          while ll1_current != None and ll2_current != None:
-          	ll1_next = ll1_current.next
-            ll2_next = ll2_current.next
-
-            ll2_current.next = ll1_next
-            ll1_current.next = ll2_current
-
-            ll1_next = ll1_current
-            ll2_next = ll2_current
-
-            ll2_current = ll2.head
-            return self
+  def merge_lists(self, linked_list2):
