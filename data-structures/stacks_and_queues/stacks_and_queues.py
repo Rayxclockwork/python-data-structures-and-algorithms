0b1aaa8caee8113c52688561f3921893008f4d66
RAVEN DELANEY <ravend17@gmail.com>
2019-12-13 15:15:44 -0800

    outline of all methods done

--- data-structures/stacks_and_queues/stacks_and_queues.py
+++ data-structures/stacks_and_queues/stacks_and_queues.py
@@ -1,1 +1,65 @@
+class Node:
+    def __init__(self, value):
+        """creates new node when called"""
+        self.value = value
+        self.next = None
 
+class Stack:
+
+    def __init__(self):
+        """creates new stack instance"""
+        self.stack = []
+        self.top = None
+
+    def push(self):
+        """pushes new node onto stack"""
+        self.stack.append(0)
+
+    def pop(self):
+        """takes top node off stack"""
+        self.stack.pop(0)
+
+    def peek(self):
+        """returns value of top node"""
+        return self.top.value
+
+    def is_empty(self):
+        """returns boolean stating whether or not stack is empty"""
+        if self.top == None:
+            return True
+        else:
+            return False
+
+class Queue:
+    def __init__(self):
+        """creates new instance of queue"""
+        self.queue = []
+        self.front = None
+
+    def enqueue(self, value):
+        """adds new node to end of queue"""
+        current = self.front
+        new_node = Node(value)
+        if current is None:
+            self.front = new_node
+        else:
+            while current.next:
+                current = current.next
+                current.next = new_node
+
+    def dequeue(self):
+        """removes node from beginning of queue"""
+        if len(self.queue) < 1:
+            return None
+        return self.queue.pop(0)
+
+    def peek(self):
+        """returns value of node from beginning of queue"""
+        return self.front.value
+
+    def is_empty(self):
+        """returns boolean stating whether or not queue is empty"""
+        if self.front == None:
+            return True
+        else:
+            return False
--- data-structures/stacks_and_queues/test_stacks_and_queues.py
+++ data-structures/stacks_and_queues/test_stacks_and_queues.py
@@ -1,1 +1,42 @@
+from stacks_and_queues import Node, Stack, Queue
+
+def test_stack_instance():
+    stack = Stack()
+    assert stack
+
+def test_queue_instance():
+    queue = Queue()
+    assert queue
+
+# def test_node_instance():
+#     node = Node()
+#     assert node
+
+def test_push():
+    pass
+
+def test_pop():
+    pass
+
+def test_stack_peek():
+    pass
+
+def test_stack_is_empty(self):
+    expected = False
+    actual = self.top != None
+    assert expected == actual
+
+def test_enqueue():
+    pass
+
+def test_dequeue():
+    pass
+
+def test_queue_peek():
+    pass
+
+def test_queue_is_empty(self):
+    expected True
+    actual = self.front == None
+    assert expected == actual
 
