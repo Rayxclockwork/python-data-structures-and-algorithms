8a131608f1c4c1581d3f7159de61efbe178b2284
RAVEN DELANEY <ravend17@gmail.com>
2019-12-17 15:13:45 -0800

    formatted

--- challenges/fifo_animal_shelter/fifo_animal_shelter.py
+++ challenges/fifo_animal_shelter/fifo_animal_shelter.py
@@ -18,39 +18,38 @@ class Cat(Animal):
 
 
 class AnimalShelter:
-	def __init__(self):
-		self.front = None
-		
-	def enqueue(self, Animal):
-		new_animal = Animal
-        
-		if self.front == None:
-			self.front = new_animal
-			return
+    def __init__(self):
+        self.front = None
 
-        # Values exist in queue
-		current = self.front
-        # Traverse the list
-		while current:
-			if current.next == None:
-				current.next = new_animal
-				return
-			current = current.next
-	
-	
-	def dequeue(self, pref=None):
-		current = self.front
-		
-		if pref is None:
-			return 'enter cat or dog'
-		while current:
-			if current.value == pref:
-				self.front = current.next
-				current.next = None
-				return current.value
-			current = current.next
+    def enqueue(self, Animal):
+        new_animal = Animal
 
-        
+        if self.front == None:
+            self.front = new_animal
+            return
+
+    # Values exist in queue
+        current = self.front
+    # Traverse the list
+        while current:
+            if current.next == None:
+                current.next = new_animal
+                return
+            current = current.next
+
+    def dequeue(self, pref=None):
+        current = self.front
+
+        if pref is None:
+            return 'enter cat or dog'
+        while current:
+            if current.value == pref:
+                self.front = current.next
+                current.next = None
+                return current.value
+            current = current.next
+
+
 shelter = AnimalShelter()
 shelter.enqueue(Cat())
 shelter.enqueue(Cat())
--- challenges/fifo_animal_shelter/test_fifo_animal_shelter.py
+++ challenges/fifo_animal_shelter/test_fifo_animal_shelter.py
@@ -3,30 +3,36 @@ import pytest
 
 
 def test_queue_instance():
-	queue = AnimalShelter()
-	assert queue
+    queue = AnimalShelter()
+    assert queue
+
 
 def test_animal():
-	animal = Animal('cat')
-	assert animal
+    animal = Animal('cat')
+    assert animal
+
 
 def test_cat():
-	cat = Cat()
-	assert cat
+    cat = Cat()
+    assert cat
+
 
 def test_dog():
-	dog = Dog()
-	assert dog
+    dog = Dog()
+    assert dog
+
 
 def test_animal_caps():
-	caps = Animal('DOG')
-	assert caps
+    caps = Animal('DOG')
+    assert caps
+
 
 def test_enqueue():
-	empty = AnimalShelter()
-	empty.enqueue('cat')
-	assert empty.front == 'cat'
+    empty = AnimalShelter()
+    empty.enqueue('cat')
+    assert empty.front == 'cat'
+
 
 def test_enqueue_bad_value():
-	with pytest.raises(TypeError):
-		Animal('lizard')
+    with pytest.raises(TypeError):
+        Animal('lizard')
