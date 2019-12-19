988b8b2f0152a91492396b19b078037a66c3a88f
RAVEN DELANEY <ravend17@gmail.com>
2019-12-19 14:28:19 -0800

    most code written

--- data-structures/tree/README.md
+++ data-structures/tree/README.md
@@ -1,9 +1,17 @@
-# Challenge Summary
+# Binary Tree & Binary Search Trees
+
+## Challenge Summary
 <!-- Short summary or background information -->
 Introduction to Binary trees and their associated methods
 
 ## Approach & Efficiency
 <!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+O(n)
 
 ## Solution
 <!-- Embedded whiteboard image -->
+pre_order
+in_order
+post_order
+add
+contains
--- data-structures/tree/test_tree.py
+++ data-structures/tree/test_tree.py
@@ -1,4 +1,5 @@
 from tree import BinaryTree, BinarySearchTree
+import pytest
 
 
 def test_tree_instance():
@@ -13,7 +14,7 @@ def test_tree_one_member():
 
 
 def test_three_members():
-    tree = BinarySearchTree():
+    tree = BinarySearchTree()
     tree.add(10)
     tree.add(5)
     tree.add(15)
@@ -24,11 +25,45 @@ def test_three_members():
 
 
 def test_pre_order():
-    tree = BinarySearchTree():
+    tree = BinarySearchTree()
     tree.add(10)
     tree.add(5)
     tree.add(15)
 
     expected = [10, 5, 15]
     actual = tree.pre_order()
-    assert actual == expected
+    assert expected == actual
+
+
+def test_in_order():
+    tree = BinarySearchTree()
+    tree.add(10)
+    tree.add(5)
+    tree.add(15)
+
+    expected = [5, 10, 15]
+    actual = tree.in_order()
+    assert expected == actual
+
+
+def test_post_order():
+    tree = BinarySearchTree()
+    tree.add(10)
+    tree.add(5)
+    tree.add(15)
+
+    expected = [5, 15, 10]
+    actual = tree.post_order()
+    assert expected == actual
+
+
+def test_adds():
+    pass
+
+
+def test_contains_true():
+    pass
+
+
+def test_contains_false():
+    pass
