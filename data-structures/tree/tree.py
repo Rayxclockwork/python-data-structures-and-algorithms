420a2ca5534cf70385c66ed45e2941f4c7ef8e6d
RAVEN DELANEY <ravend17@gmail.com>
2019-12-19 15:45:58 -0800

    fixed add method

--- data-structures/tree/tree.py
+++ data-structures/tree/tree.py
@@ -60,11 +60,10 @@ class BinarySearchTree(BinaryTree):
     def add(self, value):
         """Adds node to a tree and places it dependent upon the rest of the tree"""
         node = _Node(value)
-        if not self._root:
+        if self._root is None:
             self._root = node
-            return
 
-        if value < self._root.value:
+        elif value < self._root.value:
             if not self._root.left:
                 self._root.left = node
 
@@ -72,6 +71,7 @@ class BinarySearchTree(BinaryTree):
             if not self._root.right:
                 self._root.right = node
         print(node.value)
+        print(self._root.value)
 
     def contains(self, value):
         """returns boolean that expresses whether or not value exists in binary tree"""
