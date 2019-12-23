bfddf646ead4458bdc664fde480bf6eb9b0ccca3
RAVEN DELANEY <ravend17@gmail.com>
2019-12-23 13:15:36 -0800

    started fizzbuzz code

--- challenges/fizz_buzz_tree/fizz_buzz_tree.py
+++ challenges/fizz_buzz_tree/fizz_buzz_tree.py
@@ -1,8 +1,34 @@
-class Node:
-	pass
+class _Node():
+    def __init__(self, value):
+        """Creates new node when called"""
+        self.value = value
+        self.left = None
+        self.right = None
 
-class BinaryTree:
-	pass
 
-	def fizz_buzz_tree(self):
-		pass
+class BinaryTree():
+	def __init__(self):
+		"""Creates new instance of Binary Tree"""
+		self._root = None
+	
+	def fizz_buzz_tree(self, node=None, arr=[]):
+		node = node or self._root
+
+		if node is None:
+			return []
+
+		current = self._root
+		while True:
+			if current.node.value % 15 == 0:
+				current.node.value = 'FizzBuzz'
+
+			if current.node.value % 3 == 0:
+				current.node.value = 'Fizz'
+
+			if current.node.value % 5 == 0:
+				current.node.value = 'Buzz'
+
+
+	## % 15, then 3, then 5 == 0
+	## loop, then modify values in given tree
+	## reassign value of node to fizz, buzz, or FizzBuzz
--- challenges/fizz_buzz_tree/test_fizz_buzz_tree.py
+++ challenges/fizz_buzz_tree/test_fizz_buzz_tree.py
@@ -1,2 +1,13 @@
 from fizz_buzz_tree import Node, BinaryTree
 import pytest
+
+
+def test_single_3():
+	
+	pass
+
+def test_single_5():
+	pass
+
+def test_15():
+	pass
