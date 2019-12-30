db0d9977a1b31ac63294fef5dfcbf04a77813647
RAVEN DELANEY <ravend17@gmail.com>
2019-12-30 09:47:27 -0800

    class review

--- challenges/fizz_buzz_tree/fizz_buzz_tree.py
+++ challenges/fizz_buzz_tree/fizz_buzz_tree.py
@@ -11,23 +11,61 @@ class BinaryTree():
 		"""Creates new instance of Binary Tree"""
 		self._root = None
 	
-	def fizz_buzz_tree(self, node=None, arr=[]):
-		node = node or self._root
+	
+def fizz_buzz_tree(tree):
+	# node = _Node or tree._root
 
-		if node is None:
-			return []
+	# if node is None:
+	# 	return []
 
-		current = self._root
-		while True:
-			if current.node.value % 15 == 0:
-				current.node.value = 'FizzBuzz'
+	# current = tree._root
+	# while True:
+	# 	if current.node.value % 15 == 0:
+	# 		current.node.value = 'FizzBuzz'
 
-			if current.node.value % 3 == 0:
-				current.node.value = 'Fizz'
+	# 	if current.node.value % 3 == 0:
+	# 		current.node.value = 'Fizz'
 
-			if current.node.value % 5 == 0:
-				current.node.value = 'Buzz'
+	# 	if current.node.value % 5 == 0:
+	# 		current.node.value = 'Buzz'
 
+	def fizzify(num):
+		adjusted_val = None
+
+		if num % 15 == 0:
+			adjusted_val = 'FizzBuzz'
+
+		if num % 3 == 0:
+			adjusted_val = 'Fizz'
+
+		if num % 5 == 0:
+			adjusted_val = 'Buzz'
+
+		if adjusted_val == None:
+			return str(num)
+		else:
+			return adjusted_val
+	
+	def traverse(root):
+		if not root:
+			return None
+
+		fizzified = fizzify(root.value)
+
+		new_node = _Node(fizzified)
+
+		new_node.left = traverse(root.left)
+
+		new_node.right = traverse(root.right)
+
+		return new_node
+	
+	
+	fizzy_tree = BinaryTree()
+
+	fizzy_tree._root = traverse(tree)
+
+	return fizzy_tree
 
 	## % 15, then 3, then 5 == 0
 	## loop, then modify values in given tree
--- challenges/fizz_buzz_tree/test_fizz_buzz_tree.py
+++ challenges/fizz_buzz_tree/test_fizz_buzz_tree.py
@@ -1,9 +1,16 @@
-from fizz_buzz_tree import Node, BinaryTree
+from fizz_buzz_tree import _Node, BinaryTree, fizz_buzz_tree
 import pytest
 
 
+def test_fizz_buzz():
+	tree = BinaryTree()
+	tree._root = _Node(3)
+	tree._root.left = _Node(9)
+	tree._root.right = _Node(15)
+	tree._root.right.left = _Node(7)
+	assert fizz_buzz_tree(tree)._root.value == 'Fizz'
+
 def test_single_3():
-	
 	pass
 
 def test_single_5():
