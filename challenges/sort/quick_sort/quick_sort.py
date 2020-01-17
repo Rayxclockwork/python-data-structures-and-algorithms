b1ff649873b71fdffdd4054d60a0a49d97fe6efd
RAVEN DELANEY <ravend17@gmail.com>
2020-01-16 17:15:30 -0800

    quick sort started

--- challenges/sort/quick_sort/quick_sort.py
+++ challenges/sort/quick_sort/quick_sort.py
@@ -21,16 +21,19 @@ def partition(arr, left_pointer, right_pointer):
 	swap(arr, left_pointer, pivot_position)
 	return left_pointer
 
+
 def swap(arr, left_index, right_index):
 	arr[left_index], arr[right_index] = arr[right_index], arr[left_index]
 
-def quick_sort(arr, left_index, right_index):
-	
-	if right_index - left_index <= 0:
-		return
-	
-	pivot_position = partition(arr, left_index, right_index)
 
-	quick_sort(arr, left_index, pivot_position-1)
-	
-	quick_sort(arr, pivot_position+1, right_index)
+if __name__ == '__main__':
+	def quick_sort(arr, left_index, right_index):
+		
+		if right_index - left_index <= 0:
+			return
+		
+		pivot_position = partition(arr, left_index, right_index)
+
+		quick_sort(arr, left_index, pivot_position-1)
+
+		quick_sort(arr, pivot_position+1, right_index)
