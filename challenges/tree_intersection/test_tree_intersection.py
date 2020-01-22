9cae3591befc3863a053431bcb3fa9ac52c4c54b
RAVEN DELANEY <ravend17@gmail.com>
2020-01-21 16:23:59 -0800

    4 out of 4 tests passing

--- challenges/tree_intersection/test_tree_intersection.py
+++ challenges/tree_intersection/test_tree_intersection.py
@@ -1,18 +1,25 @@
-from tree_intersection import Hashtable, _Node, Queue, BinaryTree, tree_intersection, recurse_traverse
+from tree_intersection import _Node, Queue, BinaryTree, tree_intersection
 import pytest
+
+
+def test_binary_tree():
+    tree_one = BinaryTree()
+    tree_one.add(12)
+    tree_one.add(5)
+    tree_one.add(3)
+    tree_one.add(15)
+    tree_two = BinaryTree()
+    tree_two.add(1)
+    tree_two.add(5)
+    tree_two.add(12)
+    tree_two.add(55)
+    assert tree_one._root.value == 12
+    assert tree_two._root.value == 1
+    assert tree_one._root.left.value == 5
 
 def test_tree_instance():
     tree = BinaryTree()
     assert tree._root is None
-
-def test_hashtable_instance():
-    hashtable = Hashtable()
-    assert hashtable
-
-def test_add():
-    test_table = Hashtable()
-    test_table.add(5, 5)
-    assert test_table.get(5) == 5
 
 def test_empty_tree():
 	tree_1 = BinaryTree()
@@ -35,6 +42,6 @@ def test_tree_intersection():
 	tree_2.add(14)
 	tree_2.add(5)
 	tree_2.add(2)
-	expected = [12, 5]
+	expected = [5, 12]
 	actual = tree_intersection(tree_1, tree_2)
 	assert actual == expected
